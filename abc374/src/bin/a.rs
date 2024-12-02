5340a611fd4ae162d250256909b0a9e928364d59
kohei-kp <cowhey1991@gmail.com>
2024-12-02 16:00:16 +0900

    add abc374 a

---
+++ abc374/src/bin/a.rs
@@ -0,0 +1,13 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        s: String
+    }
+
+    if s.ends_with("san") {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
