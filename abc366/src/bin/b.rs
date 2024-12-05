9e8253366c6fc0f3570b08dd896bab2fe8b2d71d
kohei-kp <cowhey1991@gmail.com>
2024-12-06 00:50:17 +0900

    add abc366 b

--- abc366/src/bin/b.rs
+++ abc366/src/bin/b.rs
@@ -3,7 +3,23 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        s: String,
+        s: [String; n],
     }
-    todo!();
+
+    let max = s.iter().max_by_key(|s| s.len()).unwrap();
+    let mut result = vec![vec!["*".to_string(); n]; max.len()];
+
+    for i in 0..n {
+        let mut j = 0;
+        for c in s[i].chars() {
+            result[j][n - 1 - i] = c.to_string();
+            j += 1;
+        }
+    }
+    for i in 0..max.len() {
+        while result[i].last().unwrap() == "*" {
+            result[i].pop();
+        }
+        println!("{}", result[i].join(""));
+    }
 }
