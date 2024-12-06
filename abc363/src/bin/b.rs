15dc6e9cab1ded46786dbfdea1821f23aa0d36fa
kohei-kp <cowhey1991@gmail.com>
2024-12-07 00:55:22 +0900

    add abc363 b

--- abc363/src/bin/b.rs
+++ abc363/src/bin/b.rs
@@ -3,7 +3,15 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        s: String,
+        t: i32,
+        p: usize,
+        mut l: [i32; n]
     }
-    todo!();
+
+    let mut ans = 0;
+    while l.iter().filter(|&x| *x >= t).count() < p {
+        l.iter_mut().for_each(|x| *x += 1);
+        ans += 1;
+    }
+    println!("{}", ans);
 }
