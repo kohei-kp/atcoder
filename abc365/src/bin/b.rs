2b30bf65b19a10f0aff2d8a5a935e7e2e881e85a
kohei-kp <cowhey1991@gmail.com>
2024-12-06 22:35:03 +0900

    add abc365 b

--- abc365/src/bin/b.rs
+++ abc365/src/bin/b.rs
@@ -3,7 +3,10 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        s: String,
+        a: [i32; n],
     }
-    todo!();
+    let mut _a = a.clone();
+    _a.sort_by(|a, b| b.cmp(a));
+
+    println!("{}", a.iter().position(|x| x == &_a[1]).unwrap() + 1);
 }
