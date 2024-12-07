f4b049cb01c066030e7a139d07b9239bb7dfef2d
kohei-kp <cowhey1991@gmail.com>
2024-12-07 19:11:54 +0900

    add abc362 b

--- abc362/src/bin/b.rs
+++ abc362/src/bin/b.rs
@@ -2,8 +2,24 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        s: String,
+        a: (i32, i32),
+        b: (i32, i32),
+        c: (i32, i32),
     }
-    todo!();
+
+    let dist_a = distance(a, b);
+    let dist_b = distance(b, c);
+    let dist_c = distance(c, a);
+
+    if dist_a == dist_b + dist_c || dist_b == dist_c + dist_a || dist_c == dist_a + dist_b {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
+
+fn distance(a: (i32, i32), b: (i32, i32)) -> i32 {
+    let dx = a.0 - b.0;
+    let dy = a.1 - b.1;
+    dx * dx + dy * dy
 }
