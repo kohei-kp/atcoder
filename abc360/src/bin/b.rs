d2b6b042b19aea7c3616869ee2fbccbad496c39a
kohei-kp <cowhey1991@gmail.com>
2024-12-08 14:38:01 +0900

    add abc360 b

--- abc360/src/bin/b.rs
+++ abc360/src/bin/b.rs
@@ -2,8 +2,29 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
         s: String,
+        t: String,
     }
-    todo!();
+
+    let chars: Vec<char> = s.chars().collect();
+
+    let mut chunk_num = 1;
+    while chunk_num < s.len() {
+        let mut chunk = vec![];
+        chars.chunks(chunk_num).for_each(|c| chunk.push(c));
+
+        let mut _s: Vec<String> = vec![String::new(); chunk_num];
+        chunk.iter().for_each(|c| {
+            for i in 0..c.len() {
+                _s[i].push(c[i]);
+            }
+        });
+
+        if _s.contains(&t) {
+            println!("Yes");
+            return;
+        }
+        chunk_num += 1;
+    }
+    println!("No");
 }
