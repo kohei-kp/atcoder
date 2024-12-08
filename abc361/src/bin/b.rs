419810c9c6ed4c9464fb7804f6fdff4fd263d490
kohei-kp <cowhey1991@gmail.com>
2024-12-08 13:39:56 +0900

    add abc361 b 解説見て解いた

--- abc361/src/bin/b.rs
+++ abc361/src/bin/b.rs
@@ -1,9 +1,47 @@
 use proconio::input;
 
-fn main() {
+struct Point {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+fn get_point() -> Point {
     input! {
-        n: usize,
-        s: String,
+        x: i32,
+        y: i32,
+        z: i32,
     }
-    todo!();
+    Point { x, y, z }
+}
+
+fn solve() -> bool {
+    let p1 = get_point();
+    let p2 = get_point();
+    let p3 = get_point();
+    let p4 = get_point();
+
+    if p2.x <= p3.x {
+        return false;
+    }
+    if p4.x <= p1.x {
+        return false;
+    }
+    if p2.y <= p3.y {
+        return false;
+    }
+    if p4.y <= p1.y {
+        return false;
+    }
+    if p2.z <= p3.z {
+        return false;
+    }
+    if p4.z <= p1.z {
+        return false;
+    }
+    true
+}
+
+fn main() {
+    println!("{}", if solve() { "Yes" } else { "No" });
 }
