d435ac916a107b88c84a1e50f06e0b2456b4c4bb
kohei-kp <cowhey1991@gmail.com>
2024-12-08 16:04:41 +0900

    add abc359 b

--- abc359/src/bin/b.rs
+++ abc359/src/bin/b.rs
@@ -3,7 +3,18 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        s: String,
+        a: [i32; n * 2],
     }
-    todo!();
+
+    let mut prev = a[0];
+    let mut count = 0;
+    for i in 1..(n * 2) - 1 {
+        let current = a[i];
+        let next = a[i + 1];
+        if prev == next && prev != current {
+            count += 1;
+        }
+        prev = current;
+    }
+    println!("{}", count);
 }
