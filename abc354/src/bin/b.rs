2800cd7825456f83ac63d829467ac93c6d46acd3
kohei-kp <cowhey1991@gmail.com>
2024-12-10 00:23:44 +0900

    add abc354 b

--- abc354/src/bin/b.rs
+++ abc354/src/bin/b.rs
@@ -3,7 +3,11 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        s: String,
+        mut pairs: [(String, i32); n]
     }
-    todo!();
+
+    pairs.sort_by(|a, b| a.0.cmp(&b.0));
+    let sum = pairs.iter().map(|x| x.1).sum::<i32>();
+    let ans = sum % n as i32;
+    println!("{}", pairs[ans as usize].0);
 }
