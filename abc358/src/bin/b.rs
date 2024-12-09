3b556de1e9ea3f4f372d4d7f9452e5ef7f78e501
kohei-kp <cowhey1991@gmail.com>
2024-12-10 00:41:14 +0900

    update abc358 b

--- abc358/src/bin/b.rs
+++ abc358/src/bin/b.rs
@@ -7,22 +7,13 @@ fn main() {
         t: [i32; n],
     }
 
-    let mut prev = t[0];
-    let mut prev_purchase_finish_time = prev + purchase_time;
-    println!("{}", prev_purchase_finish_time);
-
-    for i in 1..n {
-        let current = t[i];
-        let diff = current - prev;
-        let purchase_diff = (prev_purchase_finish_time - current).abs();
-        let purchase_finish_time = if diff <= purchase_time {
-            prev_purchase_finish_time + purchase_time
+    let mut x = 0;
+    for i in 0..n {
+        if t[i] < x {
+            x = x + purchase_time;
         } else {
-            prev_purchase_finish_time + purchase_diff + purchase_time
-        };
-        println!("{}", purchase_finish_time);
-
-        prev = current;
-        prev_purchase_finish_time = purchase_finish_time;
+            x = t[i] + purchase_time;
+        }
+        println!("{}", x);
     }
 }
