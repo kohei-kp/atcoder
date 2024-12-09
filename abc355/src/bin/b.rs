b38dfc91c00fc49a8db4a563f74a2395b577ba1c
kohei-kp <cowhey1991@gmail.com>
2024-12-09 23:52:35 +0900

    add abc355 b

--- abc355/src/bin/b.rs
+++ abc355/src/bin/b.rs
@@ -3,7 +3,31 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        s: String,
+        m: usize,
+        a: [i32; n],
+        b: [i32; m],
     }
-    todo!();
+
+    let mut c = [a.clone(), b.clone()].concat();
+    c.sort();
+
+    let mut count = 0;
+    let mut counted = false;
+    for i in 0..c.len() {
+        for a_i in 0..n {
+            if c[i] == a[a_i] {
+                count += 1;
+                counted = true;
+                break;
+            }
+            if counted && a_i == n - 1 {
+                count = 0;
+                counted = false;
+            }
+        }
+        if count == 2 {
+            break;
+        }
+    }
+    println!("{}", if count == 2 { "Yes" } else { "No" });
 }
