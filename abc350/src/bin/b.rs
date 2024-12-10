381fa7435cb869ee4d4c293598892abfd11460d3
kohei-kp <cowhey1991@gmail.com>
2024-12-10 22:43:03 +0900

    update abc350 b

--- abc350/src/bin/b.rs
+++ abc350/src/bin/b.rs
@@ -2,8 +2,22 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        s: String,
+        mut n: i32,
+        q: usize,
+        t: [i32; q],
     }
-    todo!();
+
+    let mut checked = std::collections::HashSet::new();
+
+    for i in 0..q {
+        if checked.contains(&t[i]) {
+            n += 1;
+            checked.remove(&t[i]);
+        } else {
+            n -= 1;
+            checked.insert(t[i]);
+        }
+    }
+
+    println!("{}", n);
 }
