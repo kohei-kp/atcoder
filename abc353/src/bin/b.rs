aa7c8a899efb0541d6ee2aef0618e0e2c20a5035
kohei-kp <cowhey1991@gmail.com>
2024-12-10 14:46:19 +0900

    add abc353 b

--- abc353/src/bin/b.rs
+++ abc353/src/bin/b.rs
@@ -3,7 +3,25 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        s: String,
+        k: i32,
+        a: [i32; n]
     }
-    todo!();
+
+    let mut count = 0;
+
+    let mut _k = k;
+
+    for i in 0..n {
+        let current_group = a[i];
+        let next_group = if i == n - 1 { 0 } else { a[i + 1] };
+
+        _k -= current_group;
+        if _k < next_group || next_group == 0 {
+            count += 1;
+            _k = k;
+        } else {
+            continue;
+        }
+    }
+    println!("{}", count);
 }
