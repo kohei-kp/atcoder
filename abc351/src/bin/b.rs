8a48b74d6c49c31bfa5d636623f7e9bd814d8742
kohei-kp <cowhey1991@gmail.com>
2024-12-10 20:01:46 +0900

    update abc351 b

--- abc351/src/bin/b.rs
+++ abc351/src/bin/b.rs
@@ -1,24 +1,15 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n: usize,
-        a: [String; n],
-        b: [String; n]
+        a: [Chars; n],
+        b: [Chars; n]
     }
-
-    let board_a = a
-        .iter()
-        .map(|x| x.chars().collect::<Vec<char>>())
-        .collect::<Vec<Vec<char>>>();
-    let board_b = b
-        .iter()
-        .map(|x| x.chars().collect::<Vec<char>>())
-        .collect::<Vec<Vec<char>>>();
 
     for i in 0..n {
         for j in 0..n {
-            if board_a[i][j] != board_b[i][j] {
+            if a[i][j] != b[i][j] {
                 println!("{} {}", i + 1, j + 1);
                 return;
             }
