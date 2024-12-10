48ec0a193024313d81f2c4aa6f5c19551ecfac14
kohei-kp <cowhey1991@gmail.com>
2024-12-10 19:39:02 +0900

    update abc352 b

--- abc352/src/bin/b.rs
+++ abc352/src/bin/b.rs
@@ -2,8 +2,28 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
         s: String,
+        t: String,
     }
-    todo!();
+    let s_c = s.chars().collect::<Vec<char>>();
+    let t_c = t.chars().collect::<Vec<char>>();
+
+    let mut ans: Vec<i32> = vec![];
+
+    let mut prev_index = 0;
+    for i in 0..s_c.len() {
+        let c = s_c[i];
+
+        for j in prev_index..t_c.len() {
+            if c == t_c[j] {
+                ans.push(j as i32 + 1);
+                prev_index = j + 1;
+                break;
+            }
+        }
+    }
+
+    for c in ans {
+        print!("{} ", c);
+    }
 }
