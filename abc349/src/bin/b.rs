818e423f7cecffb9c5e9aa3e13b176523512de1d
kohei-kp <cowhey1991@gmail.com>
2024-12-10 23:53:54 +0900

    update abc349 b

--- abc349/src/bin/b.rs
+++ abc349/src/bin/b.rs
@@ -1,9 +1,25 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
-        s: String,
+        s: Chars,
     }
-    todo!();
+
+    let mut map = std::collections::HashMap::new();
+
+    for c in s {
+        *map.entry(c).or_insert(0) += 1;
+    }
+
+    let mut map2 = std::collections::HashMap::new();
+    for (_, v) in map {
+        *map2.entry(v).or_insert(0) += 1;
+    }
+
+    let values = map2.values().collect::<Vec<_>>();
+    if values.iter().all(|&v| v == &2) || values.is_empty() {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
