3da39cd7f4bb87ae0c52f319b698efc50612addb
kohei-kp <cowhey1991@gmail.com>
2024-12-12 17:28:44 +0900

    add abc347 b

--- abc347/src/bin/b.rs
+++ abc347/src/bin/b.rs
@@ -5,9 +5,16 @@ fn main() {
         s: Chars
     }
 
-    let mut ans = 0;
+    let mut ans: Vec<String> = vec![];
 
-    for i in (0..s.len()).rev() {
-        println!("i: {}", i);
+    for i in 0..s.len() {
+        for j in i + 1..s.len() + 1 {
+            let _s = &s[i..j];
+            ans.push(_s.iter().collect());
+        }
     }
+
+    ans.sort();
+    ans.dedup();
+    println!("{}", ans.len());
 }
