696b21052ec9f177cbf242bda85f064b9528ba86
kohei-kp <cowhey1991@gmail.com>
2024-12-15 14:01:56 +0900

    update abc384 d 解説見て実装

--- abc384/src/bin/d.rs
+++ abc384/src/bin/d.rs
@@ -1,32 +1,33 @@
 use proconio::input;
+use std::collections::BTreeSet;
 
 fn main() {
     input! {
         n: usize,
         mut s: i64,
-        a: [i64; n],
+        mut a: [i64; n],
     }
 
-    let mut sums: Vec<i64> = vec![];
+    let sum = a.iter().sum::<i64>();
+    let s = s % sum;
 
-    for i in 0..n {
-        for j in i..n {
-            sums.push(a[i..=j].iter().sum());
+    a.extend_from_within(..);
+
+    let mut prefix_sum = BTreeSet::new();
+    prefix_sum.insert(0i64);
+    let mut p = 0i64;
+
+    for i in 0..n * 2 {
+        p += a[i];
+        prefix_sum.insert(p);
+    }
+
+    for &p in &prefix_sum {
+        if prefix_sum.contains(&(p + s)) {
+            println!("Yes");
+            return;
         }
     }
-    sums.sort_by(|a, b| b.cmp(a));
 
-    let mut i = 0;
-    while i < sums.len() && s > 0 {
-        loop {
-            if s - sums[i] < 0 {
-                i += 1;
-                break;
-            }
-            s -= sums[i];
-            println!("{}", s);
-        }
-    }
-
-    println!("{}", if s == 0 { "Yes" } else { "No" });
+    println!("No");
 }
