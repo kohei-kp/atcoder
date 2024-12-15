c9e59ef583d8ba80b5c0ff70dd05ac63965a9afb
kohei-kp <cowhey1991@gmail.com>
2024-12-16 00:24:21 +0900

    update abc384 cをBit全探索で再実装

--- abc384/src/bin/c.rs
+++ abc384/src/bin/c.rs
@@ -1,40 +1,29 @@
 use proconio::input;
 
 fn main() {
+    let n = 5;
     input! {
-        a: i32,
-        b: i32,
-        c: i32,
-        d: i32,
-        e: i32,
+        a: [i32; n],
     }
 
-    let users = vec![
-        "ABCDE", "BCDE", "ACDE", "ABDE", "ABCE", "ABCD", "CDE", "BDE", "ADE", "BCE", "ACE", "BCD",
-        "ABE", "ACD", "ABD", "ABC", "DE", "CE", "BE", "CD", "AE", "BD", "AD", "BC", "AC", "AB",
-        "E", "D", "C", "B", "A",
-    ];
+    let mut ranking: Vec<(i32, String)> = vec![];
 
-    let mut ans = users
-        .iter()
-        .map(|user| {
-            let mut sum = 0;
-            user.chars().for_each(|ch| {
-                sum += match ch {
-                    'A' => a,
-                    'B' => b,
-                    'C' => c,
-                    'D' => d,
-                    'E' => e,
-                    _ => 0,
-                }
-            });
-            (*user, sum)
-        })
-        .collect::<Vec<(&str, i32)>>();
+    for s in 1..=31 {
+        let mut name = "".to_string();
+        let mut score = 0;
 
-    ans.sort_by(|a, b| b.1.cmp(&a.1).then(a.0.cmp(&b.0)));
-    ans.iter().for_each(|(user, _)| {
-        println!("{}", user);
+        for i in 0..n {
+            if s >> i & 1 != 0 {
+                score += a[i];
+                name.push_str(&format!("{}", (b'A' + i as u8) as char));
+            }
+        }
+        ranking.push((-score, name));
+    }
+
+    ranking.sort();
+
+    ranking.iter().for_each(|(_, name)| {
+        println!("{}", name);
     });
 }
