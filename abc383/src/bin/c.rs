4421ce7ea7cc503ec7192d1d9ca0bf7e5971faf5
kohei-kp <cowhey1991@gmail.com>
2024-12-17 23:43:26 +0900

    add abc383 c 解説見つつ実装 多始点BFS

--- abc383/src/bin/c.rs
+++ abc383/src/bin/c.rs
@@ -1,9 +1,59 @@
-use proconio::input;
+use std::collections::VecDeque;
+
+use proconio::{input, marker::Chars};
+
+const DI: [i32; 4] = [-1, 0, 1, 0];
+const DJ: [i32; 4] = [0, -1, 0, 1];
 
 fn main() {
     input! {
-        n: usize,
-        s: String,
+        h: usize,
+        w: usize,
+        d: i32,
+        board: [Chars; h],
     }
-    todo!();
+
+    let inf = 1_000_000_001;
+    let mut dist = vec![vec![inf; w]; h];
+    let mut queue: VecDeque<(i32, i32)> = VecDeque::new();
+
+    for i in 0..h {
+        for j in 0..w {
+            if board[i][j] == 'H' {
+                dist[i][j] = 0;
+                queue.push_back((i as i32, j as i32));
+            }
+        }
+    }
+
+    while !queue.is_empty() {
+        let (i, j) = queue.pop_front().unwrap();
+        let d = dist[i as usize][j as usize];
+        for k in 0..4 {
+            let ni = i + DI[k];
+            let nj = j + DJ[k];
+            if ni < 0 || nj < 0 || ni >= h as i32 || nj >= w as i32 {
+                continue;
+            }
+            if board[ni as usize][nj as usize] == '#' {
+                continue;
+            }
+            if dist[ni as usize][nj as usize] != inf {
+                continue;
+            }
+            dist[ni as usize][nj as usize] = d + 1;
+            queue.push_back((ni, nj));
+        }
+    }
+
+    let mut ans = 0;
+    for i in 0..h {
+        for j in 0..w {
+            if dist[i][j] <= d {
+                ans += 1;
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
