fbaf6810e763abd80bbc8ffd9d29653e9d74ce4d
kohei-kp <cowhey1991@gmail.com>
2025-01-16 00:07:44 +0900

    add abc388 b

--- abc388/src/bin/b.rs
+++ abc388/src/bin/b.rs
@@ -3,7 +3,24 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        s: String,
+        d: usize,
     }
-    todo!();
+
+    let mut snakes = vec![];
+
+    for _ in 0..n {
+        input! {
+            t: i32,
+            l: i32,
+        }
+        snakes.push((t, l));
+    }
+
+    for k in 1..=d {
+        let mut ans = vec![];
+        for (t, l) in snakes.iter() {
+            ans.push(t * (l + k as i32));
+        }
+        println!("{}", ans.iter().max().unwrap());
+    }
 }
