4a831f310589362d4f5bf11fdd91b5d79aa5739a
kohei-kp <cowhey1991@gmail.com>
2025-01-16 23:53:19 +0900

    add abc388 c

--- abc388/src/bin/c.rs
+++ abc388/src/bin/c.rs
@@ -3,7 +3,25 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        s: String,
+        a: [i32; n],
     }
-    todo!();
+
+    let mut ans = 0;
+
+    for i in 0..n {
+        let target = a[i] / 2;
+        let r = a
+            .binary_search_by(|v| {
+                if v <= &target {
+                    std::cmp::Ordering::Less
+                } else {
+                    std::cmp::Ordering::Greater
+                }
+            })
+            .unwrap_or_else(|pos| pos);
+
+        ans += r;
+    }
+
+    println!("{}", ans);
 }
