4f198f6c2166cfb2c79b59e92f4486fa787c38ec
kohei-kp <cowhey1991@gmail.com>
2025-02-02 00:22:57 +0900

    add abc391 c 解説AC

--- abc391/src/bin/c.rs
+++ abc391/src/bin/c.rs
@@ -3,7 +3,50 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        s: String,
+        q: i64
     }
-    todo!();
+
+    let mut hole = vec![];
+    for i in 0..n {
+        hole.push(i);
+    }
+    let mut cnt = vec![1; n];
+    let mut ans = 0;
+
+    for _ in 0..q {
+        input! {
+            query_type: usize,
+        }
+
+        match query_type {
+            1 => {
+                input! {
+                    mut p: usize,
+                    mut h: usize,
+                }
+                p -= 1;
+                h -= 1;
+
+                if cnt[hole[p]] >= 2 {
+                    ans -= 1;
+                }
+                cnt[hole[p]] -= 1;
+                if cnt[hole[p]] >= 2 {
+                    ans += 1;
+                }
+                hole[p] = h;
+                if cnt[hole[p]] >= 2 {
+                    ans -= 1;
+                }
+                cnt[hole[p]] += 1;
+                if cnt[hole[p]] >= 2 {
+                    ans += 1;
+                }
+            }
+            2 => {
+                println!("{}", ans);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
