a4f5d4cb11bb63892a5aa7f111f2454b390ca3b0
kohei-kp <cowhey1991@gmail.com>
2025-02-21 00:09:07 +0900

    add abc393 b

--- abc393/src/bin/b.rs
+++ abc393/src/bin/b.rs
@@ -1,9 +1,20 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
-        s: String,
+        s: Chars
     }
-    todo!();
+
+    let mut ans = 0;
+    for i in 0..s.len() {
+        for j in i..s.len() {
+            for k in j..s.len() {
+                if j - i == k - j && s[i] == 'A' && s[j] == 'B' && s[k] == 'C' {
+                    ans += 1;
+                }
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
