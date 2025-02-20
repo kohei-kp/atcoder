c7f0f4089e96c03ade4bec9a970a1b6ed6251391
kohei-kp <cowhey1991@gmail.com>
2025-02-20 23:59:50 +0900

    add abc393 a

---
+++ abc393/src/bin/a.rs
@@ -0,0 +1,18 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        s1: String,
+        s2: String,
+    }
+
+    if s1 == "sick" && s2 == "sick" {
+        println!("1");
+    } else if s1 == "fine" && s2 == "fine" {
+        println!("4");
+    } else if s1 == "sick" && s2 == "fine" {
+        println!("2");
+    } else if s1 == "fine" && s2 == "sick" {
+        println!("3");
+    }
+}
