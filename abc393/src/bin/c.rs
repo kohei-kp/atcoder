48998129aea86223d3c37ade8ec841acbdded018
kohei-kp <cowhey1991@gmail.com>
2025-02-21 00:30:05 +0900

    add abc393 c

--- abc393/src/bin/c.rs
+++ abc393/src/bin/c.rs
@@ -1,9 +1,29 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        s: String,
+        m: usize,
     }
-    todo!();
+
+    let mut sides = HashSet::new();
+
+    let mut ans = 0;
+
+    for _ in 0..m {
+        input! {
+            a: i64,
+            b: i64,
+        }
+
+        if a == b || sides.contains(&format!("{}-{}", a.min(b), a.max(b))) {
+            ans += 1;
+        } else {
+            sides.insert(format!("{}-{}", a.min(b), a.max(b)));
+        }
+    }
+
+    println!("{}", ans);
 }
