7db69292e851e31169cd9913937e7a9aef488695
kohei-kp <cowhey1991@gmail.com>
2025-02-23 00:28:43 +0900

    add abc394 d 解説AC

--- abc394/src/bin/d.rs
+++ abc394/src/bin/d.rs
@@ -1,9 +1,48 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
-        s: String,
+        s: Chars
     }
-    todo!();
+
+    println!("{}", if solve(s) { "Yes" } else { "No" });
+}
+
+fn solve(s: Vec<char>) -> bool {
+    let mut stack = Vec::new();
+
+    for c in s {
+        match c {
+            ')' => {
+                if !pop(&mut stack, '(') {
+                    return false;
+                }
+            }
+            ']' => {
+                if !pop(&mut stack, '[') {
+                    return false;
+                }
+            }
+            '>' => {
+                if !pop(&mut stack, '<') {
+                    return false;
+                }
+            }
+            _ => {
+                stack.push(c);
+            }
+        };
+    }
+    stack.is_empty()
+}
+
+fn pop(stack: &mut Vec<char>, c: char) -> bool {
+    if stack.is_empty() {
+        return false;
+    }
+    if stack.last().unwrap() != &c {
+        return false;
+    }
+    stack.pop();
+    true
 }
