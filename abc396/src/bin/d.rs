9ef83443fb221aa6cf8a91c848ba46124e00ff77
kohei-kp <cowhey1991@gmail.com>
2025-03-09 12:15:55 +0900

    add abc396 d 解説AC

--- abc396/src/bin/d.rs
+++ abc396/src/bin/d.rs
@@ -3,7 +3,43 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        s: String,
+        m: usize,
     }
-    todo!();
+
+    let mut graph: Vec<Vec<(usize, i64)>> = vec![vec![]; n];
+
+    for _ in 0..m {
+        input! {
+            mut a: usize,
+            mut b: usize,
+            w: i64,
+        }
+        a -= 1;
+        b -= 1;
+        graph[a].push((b, w));
+        graph[b].push((a, w));
+    }
+
+    let mut ans = i64::MAX;
+    let mut stack = vec![(0, vec![false; n], 0)];
+
+    while let Some((node, mut visited, x)) = stack.pop() {
+        if visited[node] {
+            continue;
+        }
+        visited[node] = true;
+
+        if node == n - 1 {
+            ans = std::cmp::min(ans, x);
+            continue;
+        }
+
+        for &(next, w) in &graph[node] {
+            if !visited[next] {
+                stack.push((next, visited.clone(), x ^ w));
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
