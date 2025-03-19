f5a4400c80338252215a820dddaa8362091d38fc
kohei-kp <cowhey1991@gmail.com>
2025-03-19 14:44:45 +0900

    add abc397 b

--- abc397/src/bin/b.rs
+++ abc397/src/bin/b.rs
@@ -2,8 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
         s: String,
     }
-    todo!();
+
+    let without_io = s
+        .split("io")
+        .filter(|&x| !x.is_empty())
+        .collect::<Vec<&str>>();
+
+    let ans: usize = without_io.iter().map(|x| x.len()).sum();
+    println!("{}", ans);
 }
