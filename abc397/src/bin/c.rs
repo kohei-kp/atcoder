ee7397bc9b1545e4ec13e51cbd3f4dea898d3685
kohei-kp <cowhey1991@gmail.com>
2025-03-19 15:59:17 +0900

    add abc397 c

--- abc397/src/bin/c.rs
+++ abc397/src/bin/c.rs
@@ -2,8 +2,42 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        s: String,
+        mut n: usize,
+        a: [i64; n],
     }
-    todo!();
+
+    let mut set = std::collections::BTreeSet::new();
+    let mut map = std::collections::BTreeMap::new();
+    let mut count = 0;
+    for i in 0..n {
+        if !set.contains(&a[i]) {
+            count += 1;
+            set.insert(a[i]);
+        }
+        map.insert(i, count);
+    }
+
+    let mut set_rev = std::collections::BTreeSet::new();
+    let mut map_rev = std::collections::BTreeMap::new();
+    count = 0;
+    for i in (0..n).rev() {
+        if !set_rev.contains(&a[i]) {
+            count += 1;
+            set_rev.insert(a[i]);
+        }
+        map_rev.insert(n - i, count);
+    }
+
+    let mut ans = 0;
+    n -= 1;
+    map.iter().for_each(|(k, v)| {
+        let rev_k = n - k;
+        if rev_k == 0 {
+            return;
+        }
+        let _ans = v + map_rev.get(&rev_k).unwrap();
+        ans = ans.max(_ans);
+    });
+
+    println!("{}", ans);
 }
