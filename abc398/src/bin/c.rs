b333b7a9229e0c251aa0b72b1f91243fa460d59d
kohei-kp <cowhey1991@gmail.com>
2025-03-29 15:18:46 +0900

    add abc398 c

--- abc398/src/bin/c.rs
+++ abc398/src/bin/c.rs
@@ -3,7 +3,30 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        s: String,
+        a: [i64; n]
     }
-    todo!();
+
+    let mut tree_map = std::collections::BTreeMap::new();
+    let mut n2p = std::collections::BTreeMap::new();
+
+    for i in 0..n {
+        n2p.insert(a[i], i);
+
+        let count = tree_map.entry(a[i]).or_insert(0);
+        *count += 1;
+    }
+
+    let mut max: i64 = 0;
+    for (k, v) in tree_map.iter_mut() {
+        if *v == 1 {
+            max = std::cmp::max(max, *k);
+        }
+    }
+
+    if max == 0 {
+        println!("-1");
+    } else {
+        let p = n2p.get(&max).unwrap();
+        println!("{}", p + 1);
+    }
 }
