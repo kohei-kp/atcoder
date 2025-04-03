7700f065f07e9dad64d8781dc048f3ecfb84fae9
kohei-kp <cowhey1991@gmail.com>
2025-04-03 23:31:28 +0900

    add abc366 c

--- abc366/src/bin/c.rs
+++ abc366/src/bin/c.rs
@@ -2,8 +2,38 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        s: String,
+        q: usize,
     }
-    todo!();
+
+    let mut bag = std::collections::HashMap::new();
+
+    for _ in 0..q {
+        input! {
+            query_type: usize,
+        }
+
+        match query_type {
+            1 => {
+                input! {
+                    x: usize,
+                }
+                bag.insert(x, bag.get(&x).unwrap_or(&0) + 1);
+            }
+            2 => {
+                input! {
+                    x: usize,
+                }
+                if let Some(count) = bag.get_mut(&x) {
+                    *count -= 1;
+                    if *count == 0 {
+                        bag.remove(&x);
+                    }
+                }
+            }
+            3 => {
+                println!("{}", bag.keys().len());
+            }
+            _ => unreachable!(),
+        }
+    }
 }
