0b436aa501d12dd5616122b0c1d5b448cff1fdd2
kohei-kp <cowhey1991@gmail.com>
2025-04-04 00:18:43 +0900

    add abc365 c 解説AC

--- abc365/src/bin/c.rs
+++ abc365/src/bin/c.rs
@@ -3,7 +3,32 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        s: String,
+        m: i64,
+        mut a: [i64; n],
     }
-    todo!();
+
+    let inf = 1001001001;
+    if f(inf, &a, m) {
+        println!("infiniite");
+    } else {
+        let mut ac = 0;
+        let mut wa = inf;
+        while ac + 1 < wa {
+            let wj = (ac + wa) / 2;
+            if f(wj, &a, m) {
+                ac = wj;
+            } else {
+                wa = wj;
+            }
+        }
+        println!("{}", ac);
+    }
+}
+
+fn f(x: i64, a: &[i64], m: i64) -> bool {
+    let mut s: i64 = 0;
+    for i in 0..a.len() {
+        s += x.min(a[i]);
+    }
+    s <= m
 }
