46e2174e3418125d67b74d83b9626507b5da5944
kohei-kp <cowhey1991@gmail.com>
2025-04-05 17:13:55 +0900

    add abc363 c 解説AC

--- abc363/src/bin/c.rs
+++ abc363/src/bin/c.rs
@@ -1,9 +1,39 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n: usize,
-        s: String,
+        k: usize,
+        mut s: Chars
     }
-    todo!();
+
+    let unique_chars = s.iter().unique().count();
+    if unique_chars == n {
+        let mut ans = 1;
+        for i in 1..=n {
+            ans *= i;
+        }
+        println!("{}", ans);
+        return;
+    }
+
+    let mut ans = 0;
+    s.sort();
+    for p in s.iter().permutations(n).unique() {
+        let mut ok = true;
+
+        for i in 0..n - k + 1 {
+            let t = p[i..i + k].iter().cloned().collect::<String>();
+            let rt = t.chars().rev().collect::<String>();
+            if t == rt {
+                ok = false;
+            }
+        }
+        if ok {
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
 }
