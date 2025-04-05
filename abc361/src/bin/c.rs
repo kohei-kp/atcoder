2d7905f95b104fa1f22decefac1adc81d83f2c58
kohei-kp <cowhey1991@gmail.com>
2025-04-05 20:57:09 +0900

    add: abc361 c 解説AC

--- abc361/src/bin/c.rs
+++ abc361/src/bin/c.rs
@@ -3,7 +3,18 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        s: String,
+        k: usize,
+        mut a: [i64; n],
     }
-    todo!();
+
+    a.sort();
+
+    let mut ans = 1e9 as i64;
+    for l in 0..k + 1 {
+        let r = l + (n - k) - 1;
+        let now = a[r] - a[l];
+        ans = ans.min(now);
+    }
+
+    println!("{}", ans);
 }
