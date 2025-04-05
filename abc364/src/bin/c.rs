68e609c1ea96a9d2d23957a5d0e9fc11c0cc5ce2
kohei-kp <cowhey1991@gmail.com>
2025-04-05 15:14:36 +0900

    add abc364 c

--- abc364/src/bin/c.rs
+++ abc364/src/bin/c.rs
@@ -3,7 +3,36 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        s: String,
+        x: i64,
+        y: i64,
+        a: [i64; n],
+        b: [i64; n],
     }
-    todo!();
+
+    let mut ab = a.into_iter().zip(b.into_iter()).collect::<Vec<_>>();
+    if x < y {
+        ab.sort_by(|a, b| a.0.cmp(&b.0));
+    } else {
+        ab.sort_by(|a, b| a.1.cmp(&b.1));
+    }
+
+    ab.reverse();
+
+    let mut sum_a = 0;
+    let mut sum_b = 0;
+    let mut ans = 0;
+
+    for (i, v) in ab.iter().enumerate() {
+        sum_a += v.0;
+        sum_b += v.1;
+        if sum_a > x || sum_b > y {
+            ans = i + 1;
+            break;
+        }
+    }
+    if ans == 0 {
+        ans = n;
+    }
+
+    println!("{}", ans);
 }
