b4126939da7ccf92ca32be684703c1b1023aac19
kohei-kp <cowhey1991@gmail.com>
2025-04-06 16:08:43 +0900

    add abc359 c 解説AC

--- abc359/src/bin/c.rs
+++ abc359/src/bin/c.rs
@@ -2,8 +2,27 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        s: String,
+        mut sx: i64,
+        sy: i64,
+        mut tx: i64,
+        ty: i64,
     }
-    todo!();
+
+    if (sx + sy) % 2 == 1 {
+        sx -= 1;
+    }
+    if (tx + ty) % 2 == 1 {
+        tx -= 1;
+    }
+
+    let x = (tx - sx).abs();
+    let y = (ty - sy).abs();
+
+    let mut ans = 0;
+    if x < y {
+        ans = y;
+    } else {
+        ans = (x + y) / 2;
+    }
+    println!("{}", ans);
 }
