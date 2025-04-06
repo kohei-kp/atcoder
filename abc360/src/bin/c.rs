5729a8b57a6f68046df74db969774c1220c1ba81
kohei-kp <cowhey1991@gmail.com>
2025-04-06 15:06:33 +0900

    add abc360 c

--- abc360/src/bin/c.rs
+++ abc360/src/bin/c.rs
@@ -1,9 +1,30 @@
+use std::collections::BTreeMap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        s: String,
+        a: [usize; n],
+        w: [usize; n],
     }
-    todo!();
+
+    let mut ws: BTreeMap<usize, Vec<usize>> = BTreeMap::new();
+
+    for i in 0..n {
+        ws.entry(a[i]).or_insert(vec![]).push(w[i]);
+    }
+
+    let empty_count = n - ws.len();
+
+    let mut ss = vec![];
+    for (_, ww) in ws.iter_mut() {
+        ww.sort();
+        for i in 0..ww.len() - 1 {
+            ss.push(ww[i]);
+        }
+    }
+    ss.sort();
+
+    println!("{:?}", ss[0..empty_count].iter().sum::<usize>());
 }
