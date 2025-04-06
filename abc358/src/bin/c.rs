83db0291bc9a895003c2ae84a8e139a7f0c0258d
kohei-kp <cowhey1991@gmail.com>
2025-04-06 18:01:22 +0900

    add abc358 c 解説AC

--- abc358/src/bin/c.rs
+++ abc358/src/bin/c.rs
@@ -1,9 +1,34 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n: usize,
-        s: String,
+        m: usize,
+        s: [Chars; n],
     }
-    todo!();
+
+    let mut ans = n;
+    // 2^n
+    for x in 0..1 << n {
+        let mut ok = true;
+
+        for i in 0..m {
+            let mut can = false;
+
+            for j in 0..n {
+                if (x >> j) & 1 == 1 && s[j][i] == 'o' {
+                    can = true;
+                }
+            }
+            if !can {
+                ok = false;
+            }
+        }
+        if ok {
+            ans = ans.min(((x as u32).count_ones()) as usize);
+        }
+    }
+
+    println!("{}", ans);
 }
