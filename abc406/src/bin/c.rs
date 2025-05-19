c46f240300c6582ed507c870ed855a2108a70827
kohei-kp <cowhey1991@gmail.com>
2025-05-19 23:14:57 +0900

    add abc406 c

--- abc406/src/bin/c.rs
+++ abc406/src/bin/c.rs
@@ -3,7 +3,53 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        s: String,
+        p: [i64; n],
     }
-    todo!();
+
+    let mut rle = vec![];
+    let mut cnt = 0;
+    let mut is_increasing = p[0] < p[1];
+    for i in 0..n - 1 {
+        if p[i] < p[i + 1] {
+            if is_increasing {
+                cnt += 1;
+                continue;
+            }
+            rle.push(('>', cnt));
+            is_increasing = true;
+            cnt = 1;
+        } else {
+            if !is_increasing {
+                cnt += 1;
+                continue;
+            }
+            rle.push(('<', cnt));
+            is_increasing = false;
+            cnt = 1;
+        }
+    }
+
+    if is_increasing {
+        rle.push(('<', cnt));
+    } else {
+        rle.push(('>', cnt));
+    }
+
+    let mut ans: i64 = 0;
+
+    for i in 0..rle.len() {
+        if rle[i].0 == '>' {
+            let mut l = 0;
+            let mut r = 0;
+            if 0 < i {
+                l = rle[i - 1].1;
+            }
+            if i + 1 < rle.len() {
+                r = rle[i + 1].1;
+            }
+            ans += l * r;
+        }
+    }
+
+    println!("{}", ans);
 }
