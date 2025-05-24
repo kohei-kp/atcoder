59521f49a17a0a00595d10025bcde46879b834fa
kohei-kp <cowhey1991@gmail.com>
2025-05-25 03:01:06 +0900

    add abc407 c

--- abc407/src/bin/c.rs
+++ abc407/src/bin/c.rs
@@ -1,9 +1,23 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
-        s: String,
+        mut s: Chars
     }
-    todo!();
+
+    let mut ans = 0;
+    let mut x = 0;
+    while let Some(c) = s.pop() {
+        loop {
+            let d = c.to_digit(10).unwrap() as i32;
+            if (d - x) % 10 == 0 {
+                break;
+            }
+            x += 1;
+            ans += 1;
+        }
+        ans += 1;
+    }
+
+    println!("{}", ans);
 }
