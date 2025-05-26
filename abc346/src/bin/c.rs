63181f35ee337c3876a2142d29156e6ef46d2faa
kohei-kp <cowhey1991@gmail.com>
2025-05-26 17:29:22 +0900

    add: abc346 c

--- abc346/src/bin/c.rs
+++ abc346/src/bin/c.rs
@@ -3,7 +3,22 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        s: String,
+        k: i64,
+        a: [i64; n],
     }
-    todo!();
+
+    let mut sum = k * (k + 1) / 2;
+    let mut set = std::collections::HashSet::new();
+
+    for i in 0..n {
+        if 1 <= a[i] && a[i] <= k {
+            if set.contains(&a[i]) {
+                continue;
+            }
+            sum -= a[i];
+            set.insert(a[i]);
+        }
+    }
+
+    println!("{}", sum);
 }
