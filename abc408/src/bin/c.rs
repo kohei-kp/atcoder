834b7fa8e89a6ada6d7f71ee255177bc0ac36d88
kohei-kp <cowhey1991@gmail.com>
2025-06-02 22:29:20 +0900

    add: abc408 c

--- abc408/src/bin/c.rs
+++ abc408/src/bin/c.rs
@@ -1,9 +1,24 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n: usize,
-        s: String,
+        m: usize,
+        range: [(Usize1, usize); m],
     }
-    todo!();
+
+    let mut walls = vec![0; n + 1];
+    for (l, r) in range {
+        walls[l] += 1;
+        walls[r] -= 1;
+    }
+    for i in 1..=n {
+        walls[i] += walls[i - 1];
+    }
+
+    let mut ans = 1_000_000_000;
+    for i in 0..n {
+        ans = ans.min(walls[i]);
+    }
+    println!("{}", ans);
 }
