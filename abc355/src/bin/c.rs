d1a3e2bce0dcf199380f64bffd0d6e609b2b6d6f
kohei-kp <cowhey1991@gmail.com>
2025-06-10 02:51:01 +0900

    add: abc355 c

--- abc355/src/bin/c.rs
+++ abc355/src/bin/c.rs
@@ -3,7 +3,39 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        s: String,
+        t: usize,
+        a: [usize; t],
     }
-    todo!();
+
+    let mut vertical = vec![n; n];
+    let mut horizontal = vec![n; n];
+    let mut cross = vec![n; 2];
+
+    let mut count = 0;
+    let mut bingo = false;
+
+    for i in 0..t {
+        let value = a[i];
+        let x = (value - 1) / n;
+        let y = (value - 1) % n;
+        vertical[x] -= 1;
+        horizontal[y] -= 1;
+        if x == y {
+            cross[0] -= 1;
+        }
+        if x + y == n - 1 {
+            cross[1] -= 1;
+        }
+        count += 1;
+        if vertical[x] == 0 || horizontal[y] == 0 || cross[0] == 0 || cross[1] == 0 {
+            bingo = true;
+            break;
+        }
+    }
+
+    if count <= t && bingo {
+        println!("{}", count);
+    } else {
+        println!("-1");
+    }
 }
