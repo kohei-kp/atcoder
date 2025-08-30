034e851035e1673397e20be32de9b386feda376d
kohei-kp <cowhey1991@gmail.com>
2025-08-30 22:44:22 +0900

    add: abc421 a b

--- abc421/src/bin/a.rs
+++ abc421/src/bin/a.rs
@@ -3,7 +3,24 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        s: String,
     }
-    todo!();
+
+    let mut s = vec![];
+    for _ in 0..n {
+        input! {
+            a: String,
+        }
+        s.push(a);
+    }
+
+    input! {
+        x: usize,
+        y: String
+    }
+
+    if s[x - 1] == y.to_string() {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
--- abc421/src/bin/b.rs
+++ abc421/src/bin/b.rs
@@ -2,8 +2,19 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        s: String,
+        x: usize,
+        y: usize
     }
-    todo!();
+
+    let mut a = vec![0; 11];
+    a[1] = x;
+    a[2] = y;
+    for i in 3..=10 {
+        a[i] = rev((a[i - 1] + a[i - 2]).to_string()).parse().unwrap();
+    }
+    println!("{}", a[10]);
+}
+
+fn rev(s: String) -> String {
+    s.chars().rev().collect()
 }
