fe1c0703fd893bf8e83c8ed64ca264c757b294d4
kohei-kp <cowhey1991@gmail.com>
2025-08-31 00:27:11 +0900

    add: abc421 c

--- abc421/src/bin/c.rs
+++ abc421/src/bin/c.rs
@@ -5,5 +5,31 @@ fn main() {
         n: usize,
         s: String,
     }
-    todo!();
+
+    // 交互になるパターンは2つ
+    // ABAB or BABA
+    let a_positions: Vec<usize> = s
+        .chars()
+        .enumerate()
+        .filter_map(|(i, c)| if c == 'A' { Some(i) } else { None })
+        .collect();
+    let b_positions: Vec<usize> = s
+        .chars()
+        .enumerate()
+        .filter_map(|(i, c)| if c == 'B' { Some(i) } else { None })
+        .collect();
+
+    let ab_moves = a_positions
+        .iter()
+        .enumerate()
+        .map(|(i, &pos)| (2 * i) as isize - pos as isize)
+        .map(|diff| diff.abs())
+        .sum::<isize>();
+    let ba_moves = b_positions
+        .iter()
+        .enumerate()
+        .map(|(i, &pos)| (2 * i) as isize - pos as isize)
+        .map(|diff| diff.abs())
+        .sum::<isize>();
+    println!("{}", ab_moves.min(ba_moves));
 }
