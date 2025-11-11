13201d7984c151128a7a70be5a0f9eeab678081f
kohei-kp <cowhey1991@gmail.com>
2025-11-11 21:57:32 +0900

    add: abc320 c解説AC

--- abc320/src/bin/c.rs
+++ abc320/src/bin/c.rs
@@ -1,9 +1,43 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
+use std::cmp::min;
 
 fn main() {
     input! {
-        n: usize,
-        s: String,
+        m: usize,
+        s1: Chars,
+        s2: Chars,
+        s3: Chars,
     }
-    todo!();
+
+    let mut ans = 10e9 as i64;
+    for t0 in 0..300 {
+        for t1 in 0..300 {
+            for t2 in 0..300 {
+                if t0 == t1 {
+                    continue;
+                }
+                if t0 == t2 {
+                    continue;
+                }
+                if t1 == t2 {
+                    continue;
+                }
+
+                if s1[t0 % m] != s2[t1 % m] {
+                    continue;
+                }
+                if s1[t0 % m] != s3[t2 % m] {
+                    continue;
+                }
+
+                let t_max = [t0 as i64, t1 as i64, t2 as i64].into_iter().max().unwrap();
+                ans = min(ans, t_max);
+            }
+        }
+    }
+    if ans == 10e9 as i64 {
+        ans = -1;
+    }
+
+    println!("{}", ans);
 }
