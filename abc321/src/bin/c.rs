c489817623cbb49a8f130f1724b3ae8474439e97
kohei-kp <cowhey1991@gmail.com>
2025-11-12 02:10:06 +0900

    add: abc321 c 解説AC

--- abc321/src/bin/c.rs
+++ abc321/src/bin/c.rs
@@ -2,8 +2,24 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        s: String,
+        k: usize,
     }
-    todo!();
+
+    let mut a: Vec<i64> = vec![];
+
+    for s in 0..(1 << 10) {
+        let mut x: i64 = 0;
+        for i in (0..10).rev() {
+            if s >> i & 1 == 1 {
+                x = x * 10 + i;
+            }
+        }
+        if x == 0 {
+            continue;
+        }
+        a.push(x);
+    }
+    a.sort();
+
+    println!("{}", a[k - 1]);
 }
