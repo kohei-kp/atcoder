290e20d7cd97f8dd6f4ed97f3dfb8c81c0adc390
kohei-kp <cowhey1991@gmail.com>
2025-11-13 21:59:48 +0900

    add: d to_string()はずした

--- adt_20251113_2/src/bin/d.rs
+++ adt_20251113_2/src/bin/d.rs
@@ -9,23 +9,23 @@ fn main() {
         l: [usize; q]
     }
 
-    let mut cell = vec!["".to_string(); n];
+    let mut cell = vec![""; n];
     for i in 0..k {
-        cell[a[i] - 1] = "#".to_string();
+        cell[a[i] - 1] = "#"
     }
 
     for i in 0..q {
         let num = l[i];
         let mut cnt = 0;
         for j in 0..cell.len() {
-            if cell[j] == "#".to_string() {
+            if cell[j] == "#" {
                 cnt += 1;
             }
             if cnt == num {
                 if j + 1 < n {
-                    if cell[j + 1] == "".to_string() {
-                        cell[j + 1] = "#".to_string();
-                        cell[j] = "".to_string();
+                    if cell[j + 1] == "" {
+                        cell[j + 1] = "#";
+                        cell[j] = "";
                     }
                 }
             }
@@ -34,7 +34,7 @@ fn main() {
 
     let mut cnt = 1;
     for i in 0..cell.len() {
-        if cell[i] == "#".to_string() {
+        if cell[i] == "#" {
             print!("{} ", cnt);
         }
         cnt += 1;
