5bc22e1fb55a39e751830ec97bd8e696f090b021
kohei-kp <cowhey1991@gmail.com>
2025-11-17 23:18:17 +0900

    add: abc430 b

--- abc430/src/bin/b.rs
+++ abc430/src/bin/b.rs
@@ -1,9 +1,28 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n: usize,
-        s: String,
+        m: usize,
+        grid: [Chars; n]
     }
-    todo!();
+
+    let mut ans = vec![];
+
+    for si in 0..n - m + 1 {
+        for sj in 0..n - m + 1 {
+            let mut sub = vec![vec!['.'; m]; m];
+            for i in 0..m {
+                for j in 0..m {
+                    sub[i][j] = grid[si + i][sj + j];
+                }
+            }
+            ans.push(sub.iter().flatten().join(""));
+        }
+    }
+
+    ans.sort();
+    ans.dedup();
+    println!("{}", ans.len());
 }
