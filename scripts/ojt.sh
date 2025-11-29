34e517a7cb4a3d920fdb3605f086e67e92c61111
kohei-kp <cowhey1991@gmail.com>
2025-11-30 02:15:54 +0900

    feat: 誤差許容用の-eオプションをojtに追加

--- scripts/ojt.sh
+++ scripts/ojt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu
 
-if [ $# -ne 2 ]; then
+if [ $# -lt 2 ]; then
     echo "Usage: ojt.sh <contest_number> <problem_letter>"
     exit 1
 fi
@@ -14,6 +14,14 @@ CONTEST="abc${NUM}"
 BIN="${CONTEST}/src/bin/${LETTER}.rs"
 TESTDIR="${CONTEST}/test/${LETTER}"
 
+# 第3引数以降で -e フラグがあれば設定
+E_OPT=""
+for arg in "${@:3}"; do
+    if [ "$arg" = "-e" ]; then
+        E_OPT="-e 0.1e-5"
+    fi
+done
+
 # サンプルが存在しない場合、自動でダウンロード
 if [ ! -d "$TESTDIR" ] || [ -z "$(ls -A "$TESTDIR")" ]; then
     echo "[INFO] Samples not found, downloading..."
@@ -21,4 +29,4 @@ if [ ! -d "$TESTDIR" ] || [ -z "$(ls -A "$TESTDIR")" ]; then
 fi
 
 echo "[INFO] Testing $BIN"
-PYTHONWARNINGS="ignore" oj t -d "$TESTDIR" -c "cargo run -p ${CONTEST} --bin ${LETTER}"
+PYTHONWARNINGS="ignore" oj t $E_OPT -d "$TESTDIR" -c "cargo run -p ${CONTEST} --bin ${LETTER}"
